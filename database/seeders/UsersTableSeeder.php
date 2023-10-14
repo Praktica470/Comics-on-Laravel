0a88dc61d710fdc743ee89de9258aa4098e382e8
semikoz.d.d <theodoreroos47@gmail.com>
2023-10-14 09:55:04 +0300

    Initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        $users = User::factory()->count(3)->create();
+    }
+}
